2d0a4fa47b96f4e7abfa6fc307d449f207fed495
Artem <pachevartem@ya.ru>
2020-07-03 12:28:39 +0300

    first commtit

---
+++ ASPStream/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ASPStream.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+ 
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Server is run";
+        }
+
+    
+
+    }
+}
---
+++ ASPStream/Hub/ServerHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPStream
+{
+    public class ServerHub : Hub
+    {
+        public ServerHub()
+        {
+            Trace.WriteLine("Eaaaa, Create new hub");
+        }
+
+
+        public void ClientRegistration(string name)
+        {
+            Trace.WriteLine($"msg: {name}");
+            if (name == "unity")
+            {
+                Pulse();
+            }
+        }
+
+        public void Pulse()
+        {
+            Clients.All.SendCoreAsync("EventName", new object[] { });
+        }
+
+        public string EchoMsg(string word)
+        {
+            return word + "-" + word;
+        }
+    }
+}
